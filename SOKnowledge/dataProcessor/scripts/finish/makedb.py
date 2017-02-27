445a2261713edf7ccf09f3ec03fbd54405f6c12c
lmw <lmwtclmwtc@163.com>
2017-02-27 16:23:23 +0800

    make db file complete

--- SOKnowledge/dataProcessor/scripts/finish/makedb.py
+++ SOKnowledge/dataProcessor/scripts/finish/makedb.py
@@ -4,7 +4,7 @@ import sqlite3
 import os
 import xml.etree.cElementTree as etree
 import logging
-"""
+
 ANATHOMY = {
     'badges': {
         'Id': 'INTEGER',
@@ -34,9 +34,9 @@ ANATHOMY = {
         'OwnerDisplayName': 'TEXT',
         'LastEditorUserId': 'INTEGER',
         'LastEditorDisplayName': 'TEXT',  # ="Rich B"
-        'LastEditDate': 'DATETIME',  #="2009-03-05T22:28:34.823"
-        'LastActivityDate': 'DATETIME',  #="2009-03-11T12:51:01.480"
-        'CommunityOwnedDate': 'DATETIME',  #(present only if post is community wikied)
+        'LastEditDate': 'DATETIME',  # ="2009-03-05T22:28:34.823"
+        'LastActivityDate': 'DATETIME',  # ="2009-03-11T12:51:01.480"
+        'CommunityOwnedDate': 'DATETIME',  # (present only if post is community wikied)
         'Title': 'TEXT',
         'Tags': 'TEXT',
         'AnswerCount': 'INTEGER',
@@ -109,75 +109,23 @@ ANATHOMY = {
         'WikiPostId': 'INTEGER'
     }
 }
-anathomy is the so data json schema,the parse from xml is base on this
-"""
-ANATHOMY = {
-    'comments': {
-        'Id': 'INTEGER',
-        'PostId': 'INTEGER',
-        'Score': 'INTEGER',
-        'Text': 'TEXT',
-        'CreationDate': 'DATETIME',
-        'UserId': 'INTEGER',
-        'UserDisplayName': 'TEXT'
-    },
-    'posts': {
-        'Id': 'INTEGER',
-        'PostTypeId': 'INTEGER',  # 1: Question, 2: Answer
-        'ParentID': 'INTEGER',  # (only present if PostTypeId is 2)
-        'AcceptedAnswerId': 'INTEGER',  # (only present if PostTypeId is 1)
-        'CreationDate': 'DATETIME',
-        'Score': 'INTEGER',
-        'ViewCount': 'INTEGER',
-        'Body': 'TEXT',
-        'OwnerUserId': 'INTEGER',  # (present only if user has not been deleted)
-        'OwnerDisplayName': 'TEXT',
-        'LastEditorUserId': 'INTEGER',
-        'LastEditorDisplayName': 'TEXT',  # ="Rich B"
-        'LastEditDate': 'DATETIME',  #="2009-03-05T22:28:34.823"
-        'LastActivityDate': 'DATETIME',  #="2009-03-11T12:51:01.480"
-        'CommunityOwnedDate': 'DATETIME',  #(present only if post is community wikied)
-        'Title': 'TEXT',
-        'Tags': 'TEXT',
-        'AnswerCount': 'INTEGER',
-        'CommentCount': 'INTEGER',
-        'FavoriteCount': 'INTEGER',
-        'ClosedDate': 'DATETIME'
-    },
-    'votes': {
-        'Id': 'INTEGER',
-        'PostId': 'INTEGER',
-        'UserId': 'INTEGER',
-        'VoteTypeId': 'INTEGER',
-        # -   1: AcceptedByOriginator
-        # -   2: UpMod
-        # -   3: DownMod
-        # -   4: Offensive
-        # -   5: Favorite
-        # -   6: Close
-        # -   7: Reopen
-        # -   8: BountyStart
-        # -   9: BountyClose
-        # -  10: Deletion
-        # -  11: Undeletion
-        # -  12: Spam
-        # -  13: InformModerator
-        'CreationDate': 'DATETIME',
-        'BountyAmount': 'INTEGER'
-    }
-}
-def dump_files(file_names, anathomy,
+
+
+def dump_files(file_names,
+               anathomy,
+               xml_file_path='.',
                dump_path='.',
                dump_database_name='so-dump.db',
+               log_filename='so-parser.log',
                create_query='CREATE TABLE IF NOT EXISTS {table} ({fields})',
-               insert_query='INSERT INTO {table} ({columns}) VALUES ({values})',
-               log_filename='so-parser.log'):
+               insert_query='INSERT INTO {table} ({columns}) VALUES ({values})'
+               ):
     logging.basicConfig(filename=os.path.join(dump_path, log_filename), level=logging.INFO)
     db = sqlite3.connect(os.path.join(dump_path, dump_database_name))
     for file in file_names:
         print
         "Opening {0}.xml".format(file)
-        with open(os.path.join(dump_path, file + '.xml')) as xml_file:
+        with open(os.path.join(xml_file_path, file + '.xml')) as xml_file:
             tree = etree.iterparse(xml_file)
             table_name = file
 
@@ -212,5 +160,62 @@ def dump_files(file_names, anathomy,
             del (tree)
 
 
+def parse_xml_db(xml_file_names,
+                 xml_file_path=".",
+                 dump_path=".",
+                 dump_database_name='so-dump.db',
+                 log_filename='so-parser.log'):
+    legal_file_names = ANATHOMY.keys()
+
+    for file_name in xml_file_names:
+        if file_name not in legal_file_names:
+            print file_name, ' is not a legal file name\n'
+            print 'following are the legal file name:\n',
+            print legal_file_names
+            return
+    dump_files(xml_file_names, ANATHOMY, xml_file_path, dump_path, dump_database_name, log_filename)
+
+
+def usage():
+    usageStr = {
+        "-h": "show the usage",
+        "--xmlpath": "the path of the xmls file dir",
+        "--xmlnames": "the name of the xml files without file type suffix,such as post,tags,votes,if there are more than one,connect the name with ','",
+        "--logname": "the name of the parse log file.default:so-parser.log",
+        "--dbname": "the name of the database file name,must end with .db.default:so-dump.db",
+        "--dbpath": "the path of the database file name",
+    }
+    team = "\n".join(['{0}  :{1}'.format(command, description) for command, description in usageStr.items()])
+    print(team)
+
+
 if __name__ == '__main__':
-    dump_files(ANATHOMY.keys(), ANATHOMY)
+    import sys, getopt
+
+    opts, args = getopt.getopt(sys.argv[1:], "h", ["xmlnames=", "logname=", "dbname=", "xmlpath=", "dbpath="])
+
+    xml_file_names = []
+    dump_path = "."
+    dump_database_name = 'so-dump.db'
+    log_filename = 'so-parser.log'
+    xml_path = "."
+    for op, value in opts:
+        if op == "--xmlnames":
+            xml_file_names = value.split(',')
+        elif op == "--dbpath":
+            dump_path = value
+        elif op == "--dbname":
+            dump_database_name = value
+        elif op == "--xmlpath":
+            xml_path = value
+        elif op == "--logname":
+            log_filename = value
+        elif op == "-h":
+            usage()
+            sys.exit()
+
+    if len(xml_file_names) > 0:
+        print xml_file_names
+        parse_xml_db(xml_file_names)
+    else:
+        print "xml file names are not setted"
---
+++ SOKnowledge/tagger/data.md
@@ -0,0 +1,18 @@
+## data format
+a sentence is divided into tokens.
+
+each line is a token ,and it tag,connected by '\t',
+
+if a line is empty,it stands for a sentence end.
+
+the tag is base on BIO format
+## original data
+the original datas are form Ye Deheng's paper.It is divided into three sets 
++ train.conll
++ test.conll
++ dev.conll
+
+## split data
++ splittrain.conll
++ splittest.conll
++ splitdev.conll
