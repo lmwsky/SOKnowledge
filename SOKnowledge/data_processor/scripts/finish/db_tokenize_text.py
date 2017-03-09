7b1f0e923cd0fa3806cd11422729c1303503558a
lmw <lmwtclmwtc@163.com>
2017-03-09 16:49:16 +0800

    complete the db_tokenize script

--- SOKnowledge/data_processor/scripts/finish/db_tokenize_text.py
+++ SOKnowledge/data_processor/scripts/finish/db_tokenize_text.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import fire
 
-from db_util import process_table_data
+from db_util import process_table_data, get_max_id
 from nlp_util import sent_word_tokenize_nltk
 from sql_util import generate_create_sql_for_tokenize_text, \
     generate_insert_sql_for_tokenize_text
@@ -34,9 +34,11 @@ def cursor_process_tokenize_text_specific_param(cursor, source_table_name, token
     row_values = []
 
     for row in cursor.fetchall():
-        new_tokenize_row_value = row_process_tokenize_text(row, tokenize_col_name, primary_key_name)
-        row_values.append(new_tokenize_row_value)
-
+        try:
+            new_tokenize_row_value = row_process_tokenize_text(row, tokenize_col_name, primary_key_name)
+            row_values.append(new_tokenize_row_value)
+        except Exception, e:
+            print e, ' happen when ', primary_key_name, '=', row[primary_key_name]
     create_sql_list = [generate_create_sql_for_tokenize_text(tokenize_table_name)]
     sql_values_dict[generate_insert_sql_for_tokenize_text(tokenize_table_name)] = row_values
 
@@ -54,6 +56,8 @@ def row_process_tokenize_text(row, tokenize_col_name, primary_key_name="Id",
 def db_process_tokenize_text(
         table_name,
         col_name,
+        step=500000,
+        max_id=None,
         primary_key_name="Id",
         dump_path='.',
         dump_database_name='so-dump.db',
@@ -69,15 +73,25 @@ def db_process_tokenize_text(
     :param log_filename: the name of log file,default 'so-parser.log'
     :return:
     """
-    cursor_process_tokenize_text_extra_params = {'source_table_name': table_name,
-                                                 'tokenize_col_name': col_name,
-                                                 'primary_key_name': primary_key_name}
-    process_table_data(table_name,
-                       cursor_process_tokenize_text,
-                       dump_path,
-                       dump_database_name,
-                       log_filename,
-                       cursor_process_tokenize_text_extra_params)
+    if max_id is None:
+        max_id = get_max_id(dump_path, dump_database_name, table_name, primary_key_name)
+    select_query = "SELECT * FROM {table}"
+    condition_clause = " where {primary_key}>{startId} AND {primary_key}<={endId}"
+    for startId in range(0, max_id, step):
+        endId = min(startId + step, max_id)
+        sql = select_query + condition_clause.format(startId=startId, endId=endId, primary_key=primary_key_name)
+
+        cursor_process_tokenize_text_extra_params = {'source_table_name': table_name,
+                                                     'tokenize_col_name': col_name,
+                                                     'primary_key_name': primary_key_name}
+        process_table_data(table_name,
+                           cursor_process_tokenize_text,
+                           dump_path,
+                           dump_database_name,
+                           log_filename,
+                           process_func_extra_params=cursor_process_tokenize_text_extra_params,
+                           select_query=sql)
+        print 'done ', startId, '-', endId
 
 
 if __name__ == '__main__':
