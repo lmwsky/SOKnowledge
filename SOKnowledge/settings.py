a39bf70ec862d960e552de8be405cf20de48c50e
lmw <lmwtclmwtc@163.com>
2017-03-09 14:37:07 +0800

    add the stackoverflow database

--- SOKnowledge/settings.py
+++ SOKnowledge/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'SOKnowledge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'so-dump.db'),
     }
 }
 
