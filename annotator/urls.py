18d9045199a2d8dd96c17dde839b1bcd275a9678
lmw <lmwtclmwtc@163.com>
2017-03-17 10:54:25 +0800

    add annotator app for django project

--- annotator/urls.py
+++ annotator/urls.py
@@ -1,4 +1,4 @@
-"""SOKnowledge URL Configuration
+"""annotator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^annotator/', include('annotator.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
