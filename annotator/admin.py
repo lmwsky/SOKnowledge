9781e156183405a0a17ac8c84bfcf91ecf74ad61
lmw <lmwtclmwtc@163.com>
2017-03-17 11:30:01 +0800

    add admin control for all so table

--- annotator/admin.py
+++ annotator/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TokenizeCodeblockForPostsCodeBlock, Posts, CodeBlockWithTokenizeCode, RemoveTagPostsBody, \
+    TokenizeRemovetagbodyForRemoveTagPostsBody
+
+admin.site.register(TokenizeCodeblockForPostsCodeBlock)
+admin.site.register(Posts)
+admin.site.register(CodeBlockWithTokenizeCode)
+admin.site.register(RemoveTagPostsBody)
+admin.site.register(TokenizeRemovetagbodyForRemoveTagPostsBody)
