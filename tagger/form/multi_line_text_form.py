b60a0c193adba09a4fbeaefb8afd01af32caeeab
lmw <lmwtclmwtc@163.com>
2017-03-29 13:18:55 +0800

    add ner tagger demo

--- tagger/form/multi_line_text_form.py
+++ tagger/form/multi_line_text_form.py
@@ -5,13 +5,13 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class MultiLineTextForm(forms.Form):
-    input_text = forms.CharField(help_text="Enter the text needed to do named entity recognition")
+    input_text = forms.CharField(help_text="Enter the text needed to do named entity recognition",widget=forms.Textarea)
 
     def clean_input_text(self):
         data = self.cleaned_data['input_text']
         data = data.strip()
         # Check text is not null.
-        if data:
+        if not data:
             raise ValidationError(_('the input must not be empty'))
 
         # Remember to always return the cleaned data.
--- tagger/views.py
+++ tagger/views.py
@@ -1,3 +1,38 @@
+import os
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from SOKnowledge.ner_util.tagger import NERTagger
+from SOKnowledge.settings import BASE_DIR
+from annotator.data_set_util import build_conll_fomat_for_sentences, \
+    build_html_fomat_for_sentences
+from form.multi_line_text_form import MultiLineTextForm
+
+__ner_tagger__ = NERTagger()
+__ner_tagger__.load_model(os.path.join(BASE_DIR,'SOKnowledge/ner_util','models/so_for_epochs50_splitwords'))
+
+def ner_tagger(request):
+    if request.method == 'POST':
+        form = MultiLineTextForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['input_text']
+            print 'text=',text
+            return HttpResponseRedirect(reverse('tagger:ner_tagger_result', args=(text,)))
+    else:
+        form = MultiLineTextForm()
+        return render(request, 'tagger/ner_tagger_form.html', {'form': form})
+
+
+def ner_tagger_result(request, text):
+    words_list, tags_list = __ner_tagger__.tag(text)
+
+    conll_format_text = build_conll_fomat_for_sentences(words_list, tags_list)
+    html_format_text = build_html_fomat_for_sentences(words_list, tags_list)
+    return render(request, 'tagger/ner_tagger_result.html',
+                  {'original_text': text,
+                   'conll_format_text': conll_format_text,
+                   'html_format_text': html_format_text})
