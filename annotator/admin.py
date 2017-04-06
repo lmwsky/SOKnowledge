88bb8fdc47a7fec3557e12cea86bb89689b33797
lmw <lmwtclmwtc@163.com>
2017-04-06 20:21:01 +0800

    add model manage to admin interface

--- annotator/admin.py
+++ annotator/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import TokenizeCodeblockForPostsCodeBlock, Posts, CodeBlockWithTokenizeCode, RemoveTagPostsBody, \
-    TokenizeRemovetagbodyForRemoveTagPostsBody
+    TokenizeRemovetagbodyForRemoveTagPostsBody, SentenceType, NamedEntityAnnotation, SentenceTypeAnnotation
 
 admin.site.register(TokenizeCodeblockForPostsCodeBlock)
 admin.site.register(Posts)
 admin.site.register(CodeBlockWithTokenizeCode)
 admin.site.register(RemoveTagPostsBody)
 admin.site.register(TokenizeRemovetagbodyForRemoveTagPostsBody)
+admin.site.register(NamedEntityAnnotation)
+admin.site.register(SentenceType)
+admin.site.register(SentenceTypeAnnotation)
