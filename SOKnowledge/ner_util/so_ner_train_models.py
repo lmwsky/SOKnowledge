15d5da1a549f2d0ee9b4946d5d51b5f78f84aeb1
isky <lmwtclmwtc@163.com>
2017-05-03 20:05:34 +0800

    train lstm-crf model for lab for compare f1 rate with CRF model,current is test train for model0

--- SOKnowledge/ner_util/so_ner_train_models.py
+++ SOKnowledge/ner_util/so_ner_train_models.py
@@ -8,15 +8,21 @@ if __name__ == "__main__":
         train_set_location = "corpus/so_ner/dataSet3/{0}/S3train{1}.conll".format(i, i)
         dev_set_location = "corpus/so_ner/dataSet3/{0}/S3dev{1}.conll".format(i, i)
         test_set_location = "corpus/so_ner/dataSet3/{0}/S3test{1}.conll".format(i, i)
-
         pre_emb = "corpus/replace_large_code_block/word_embedding.txt"
 
+        print "trainset:",train_set_location
+        print  "devset:",dev_set_location
+        print "test:",test_set_location
+        print "pre_emb:",pre_emb
+        model_name = "SO_LSTM_CRF{0}".format(i)
+        print "model_name:",model_name
+
         trainer.init_model_parameters(train_set_location=train_set_location,
-                                  dev_set_location=dev_set_location,
-                                  test_set_location=test_set_location,
-                                  pre_emb=pre_emb
-                                  )
-        trainer.init_model("SO_LSTM_CRF{0}".format(i))
+                                      dev_set_location=dev_set_location,
+                                      test_set_location=test_set_location,
+                                      pre_emb=pre_emb
+                                      )
+        trainer.init_model(model_name)
         best_dev,best_test=trainer.train(n_epochs=50, freq_eval=300)
 
         print "-----------------------------"
