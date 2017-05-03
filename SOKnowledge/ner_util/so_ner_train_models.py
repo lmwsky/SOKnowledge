79de38ef73afc69f11b496821016a766fb699d88
isky <lmwtclmwtc@163.com>
2017-05-03 19:09:54 +0800

    train lstm-crf model for lab for compare f1 rate with CRF model,current is test train for model0

---
+++ SOKnowledge/ner_util/so_ner_train_models.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from train import NERTaggerTrainer
+if __name__ == "__main__":
+
+    for i in range(0,10):
+        trainer = NERTaggerTrainer()
+        train_set_location = "corpus/so_ner/{0}/S3train{1}.conll".format(i, i)
+        dev_set_location = "corpus/so_ner/{0}/S3dev{1}.conll".format(i, i)
+        test_set_location = "corpus/so_ner/{0}/S3test{1}.conll".format(i, i)
+
+        dev_set_location = "corpus/so_ner/0/S3dev0.conll"
+        test_set_location = "corpus/so_ner/0/S3test0.conll"
+        pre_emb = "corpus/replace_large_code_block/word_embedding.txt"
+
+        trainer.init_model_parameters(train_set_location=train_set_location,
+                                  dev_set_location=dev_set_location,
+                                  test_set_location=test_set_location,
+                                  pre_emb=pre_emb
+                                  )
+        trainer.init_model("SO_LSTM_CRF{0}".format(i))
+        best_dev,best_test=trainer.train(n_epochs=50, freq_eval=300)
+
+        print "-----------------------------"
+        print "Model{0} training finish".format(str(i))
+        print "best_dev:"+best_dev
+        print "best_test:"+best_test
+        print "-----------------------------"
